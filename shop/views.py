6b3d3d23ec3ba5106cce3c0b9608d6c0c72cf7a6
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2020-10-16 01:58:04 +0630

    animation fixes, optimization, and pagination is added

--- shop/views.py
+++ shop/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import redirect, render
 from django.http.response import HttpResponse
+from django.core.paginator import Paginator
 from .forms import *
 from .models import *
 import base64
 
 
 def shop(request):
-    item = Item.objects.all()
-    tags = Tag.objects.all()
-    
-    
-    context = {'item':item,
-                'tags':tags}
-    return render(request, "shop/shop.html", context)
+    return get_by_tag(request,'all')
 
 def get_by_tag(request,tag):
-    search_tag = Tag.objects.get(name=tag)
-    item = search_tag.item_set.all()
+    page_num = request.GET.get('page',1)
+    search_tag = None
+    if tag == 'all':
+        item = Paginator(Item.objects.all(),16)
+        item = item.page(page_num)
+    else:
+        search_tag = Tag.objects.get(name=tag)
+        item = Paginator(search_tag.item_set.all(),16)
+        item = item.page(page_num)
+
+    page_num = request.GET.get('page',1)
+    
+
     tags = Tag.objects.all()
     context = {'item':item,
                 'tags':tags,
